d0a4ff0faf0b5c801bbcbe1b8068ac9917b43b21
JTG SYSTEMS <jtgsystems@gmail.com>
2022-12-17 00:42:58 -0500

    Create install popular python libraries.bat

---
+++ install popular python libraries.bat
@@ -0,0 +1,44 @@
+@echo off
+
+echo Installing NumPy...
+rem NumPy is a library for numerical computing with Python, providing support for large, multi-dimensional arrays and matrices, along with a large collection of mathematical functions to operate on these arrays.
+pip install numpy
+
+echo Installing Pandas...
+rem Pandas is a library for data manipulation and analysis, featuring data structures for storing and manipulating large and complex datasets.
+pip install pandas
+
+echo Installing Matplotlib...
+rem Matplotlib is a library for creating static, animated, and interactive visualizations in Python.
+pip install matplotlib
+
+echo Installing SciPy...
+rem SciPy is a library for scientific and technical computing, including functions for optimization, linear algebra, and statistical analysis.
+pip install scipy
+
+echo Installing scikit-learn...
+rem scikit-learn is a library for machine learning, providing a range of algorithms and tools for training, testing, and evaluating models.
+pip install scikit-learn
+
+echo Installing TensorFlow...
+rem TensorFlow is a library for machine learning and deep learning, developed by Google.
+pip install tensorflow
+
+echo Installing Keras...
+rem Keras is a high-level library for building and training deep learning models, built on top of TensorFlow.
+pip install keras
+
+echo Installing PyTorch...
+rem PyTorch is a library for machine learning and deep learning, developed by Facebook.
+pip install torch
+
+echo Installing requests...
+rem requests is a library for making HTTP requests in Python.
+pip install requests
+
+echo Installing Beautiful Soup...
+rem Beautiful Soup is a library for parsing and navigating HTML and XML documents.
+pip install beautifulsoup4
+
+echo Installation complete.
+pause
